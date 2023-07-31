9d26ba2ad33572764b071cab76a2fa8a81119ed8
Quaniscoding <vq.2509.2003@gmail.com>
2023-07-31 18:15:29 +0700

    done

--- src/controllers/Phong/deletePhongThue.js
+++ src/controllers/Phong/deletePhongThue.js
@@ -1,34 +1,34 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const { sucessCode, failCode, errorCode } = require("../../config/reponse");
-const deletePhongThue = async (req, res) => {
-    let id = req.params.id;
-    try {
-        let checkPhongThue = await prisma.phong.findFirst({
-            where: {
-                id: Number(id)
-            }
-        })
-        if (!checkPhongThue) {
-            failCode(res, "", "Xóa phòng thuê thất bại !");
-        }
-        else {
-            const data = await prisma.phong.delete({
-                where: {
-                    id: Number(id),
-                },
-            });
-            if (data == "") {
-                failCode(res, "", "Xóa phòng thuê thất bại !");
-            } else {
-                sucessCode(res, data, "Xóa phòng thuê thành công !");
-            }
-        }
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
+// const { sucessCode, failCode, errorCode } = require("../../config/reponse");
+// const deletePhongThue = async (req, res) => {
+//     let id = req.params.id;
+//     try {
+//         let checkPhongThue = await prisma.phong.findFirst({
+//             where: {
+//                 id: Number(id)
+//             }
+//         })
+//         if (!checkPhongThue) {
+//             failCode(res, "", "Xóa phòng thuê thất bại !");
+//         }
+//         else {
+//             const data = await prisma.phong.delete({
+//                 where: {
+//                     id: Number(id),
+//                 },
+//             });
+//             if (data == "") {
+//                 failCode(res, "", "Xóa phòng thuê thất bại !");
+//             } else {
+//                 sucessCode(res, data, "Xóa phòng thuê thành công !");
+//             }
+//         }
 
-    } catch (error) {
-        errorCode(res, "Lỗi Backend");
-    }
-};
-module.exports = {
-    deletePhongThue,
-};
+//     } catch (error) {
+//         errorCode(res, "Lỗi Backend");
+//     }
+// };
+// module.exports = {
+//     deletePhongThue,
+// };
--- src/routes/Phong/deletePhongThue.js
+++ src/routes/Phong/deletePhongThue.js
@@ -1,10 +1,10 @@
-const express = require('express');
+// const express = require('express');
 
-const { deletePhongThue } = require('../../controllers/phong/deletePhongThue');
-const { verifyToken } = require('../../middlewares/baseToken');
-const userRoute = express.Router();
+// const { deletePhongThue } = require('../../controllers/phong/deletePhongThue');
+// const { verifyToken } = require('../../middlewares/baseToken');
+// const userRoute = express.Router();
 
-//http://localhost:8080/api/delete-phong-thue/{id}
-//delete
-userRoute.delete("/delete-phong-thue/:id", verifyToken, deletePhongThue)
-module.exports = userRoute;
+// //http://localhost:8080/api/delete-phong-thue/{id}
+// //delete
+// userRoute.delete("/delete-phong-thue/:id", verifyToken, deletePhongThue)
+// module.exports = userRoute;
